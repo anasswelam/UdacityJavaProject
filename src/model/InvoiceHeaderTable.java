ed4c18229d670697cec1ef1114abe1dd784bd6b1
anasswelam <anasswelam@gmail.com>
2022-10-25 14:26:32 +0200

    First commit

---
+++ src/model/InvoiceHeaderTable.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import view.InvoiceFrame;
+
+/**
+ *
+ * @author GFI
+ */
+public class InvoiceHeaderTable extends AbstractTableModel{
+    
+private static final long serialVersionUID = -1956022147958826252L;
+	private ArrayList<InvoiceHeader> invoicesArray;
+    private String[] columns = {"Invoice Num", "Invoice Date", "Customer Name", "Invoice Total"};
+    
+    public InvoiceHeaderTable(ArrayList<InvoiceHeader> invoicesArray) {
+        this.invoicesArray = invoicesArray;
+    }
+
+    @Override
+    public int getRowCount() {
+        return invoicesArray.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        InvoiceHeader inv = invoicesArray.get(rowIndex);
+        switch (columnIndex) {
+            case 0: return inv.getNum();
+            case 1: return InvoiceFrame.dateFormat.format(inv.getInvDate());
+            case 2: return inv.getCustomer();
+            case 3: return inv.getInvoiceTotal();
+        }
+        return "";
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columns[column];
+    }
+}
+
+
